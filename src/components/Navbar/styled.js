988dd08bc25b70d047abc4cb84110b044b5474e8
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-05-22 20:27:48 -0300

    RightWrapper implemented

--- src/components/Navbar/styled.js
+++ src/components/Navbar/styled.js
@@ -1,5 +1,7 @@
 import styled from "styled-components"
 
+import logo from './onda-sonora.png';
+
 export const Navbar = styled.nav`
     position:relative;
     z-index: 2;
@@ -14,13 +16,13 @@ export const Navbar = styled.nav`
 `
 export const Logo = styled.div`
     position: relative;
-    background-image: url(${brand});
+    background-image: url(${logo});
     background-position: center; 
     background-size: contain;
     background-repeat: no-repeat;
     height: 100%;
-    width: 15%;
-    margin: 0 3rem;
+    width: 25%;
+    margin: 0 3em;
 `
 export const MenuWrapper = styled.div`
     position: relative;
@@ -59,6 +61,7 @@ export const RightWrapper = styled.div`
     position: relative;
     display: grid;
     grid: 1fr 1fr / 1fr;
+    width: 25%;
 `
 
 export const SignWrapper = styled.div`
@@ -69,7 +72,7 @@ export const SignWrapper = styled.div`
     align-items: center;
     flex-grow: 1;
     height: 100%;
-    margin-left: 5rem;
+    margin-left: 5em;
 `
 
 export const Sign = styled.a`
@@ -90,7 +93,7 @@ export const Sign = styled.a`
 
     color: white;
     text-transform: uppercase; 
-    font-size: 1.2rem;
+    font-size: 1.2em;
     font-family: 'Bebas Neue', sans-serif;
 
     transition: background 0.5s ease-in-out;
@@ -101,12 +104,60 @@ export const Sign = styled.a`
     }
 `
 
-export const SearchWrapper = styled.div`
+export const SearchForm = styled.form`
+    display: flex;
+    width: 100%;
+    display: inline-block;
+    position: relative;
+    filter: drop-shadow(0 1px #0091c2);
+
+    &:after{
+        position: absolute;
+        top: 40px;
+        right: 2px;
+        content: "";
+        background: black;
+        width: 4px;
+        height: 20px;
+        transform: rotate(135deg);
+    }
+`
+export const SearchBar = styled.input`
+    position: absolute;
+    right: 0;
+
+    overflow: hidden;
+    width:  2em;
+    height: 2em;
+    padding: 1em;
+
+    color: black;
+    background: transparent;
+    outline: none;
+    border: solid 3px white;
+    border-radius: 3em;
+    transition: width 0.5s;
+
+    &:focus{
+        width: 100%;
+    }
 
 `
-export const SearchBar = styled.div`
-    
-`
-export const SearchButton = styled.div`
-    
+export const SearchButton = styled.button`
+    border: 0;
+    padding: 0.5rem;
+    border-radius: 8px;
+    position: absolute; 
+    top: 0;
+    left: 100%;
+    width: 4em;
+    transition: 0.2s;
+    background: #455A64;
+    color: white;
+    font-size: 13px;
+    height: 100%;
+
+    ${SearchBar}:focus &{
+        transform: translateX(calc(-1 * 4em));
+    }
 `

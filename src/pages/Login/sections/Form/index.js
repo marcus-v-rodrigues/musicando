923cb841b821887fa005368542ccddaf03f4f315
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-05-26 19:54:36 -0300

    Login route implemented

--- src/pages/Login/sections/Form/index.js
+++ src/pages/Login/sections/Form/index.js
@@ -5,9 +5,34 @@ import * as S from './styled'
 const Form = () => {
   return (
     <S.Container>
+        <S.Title>Login</S.Title>
         <S.FormBackground>
             <S.FormWrapper>
+
+                <S.FormGroup>
+                    <S.Label for="email">
+                        Email
+                    <S.Field type="email" name="email" id="email"  placeholder="email@dominio.com" />
+                    </S.Label>
+                </S.FormGroup>
+
+                <S.FormGroup>
+                    <S.Label for="password">
+                        Senha
+                    <S.Field type="password" name="password" id="password" placeholder="Sua senha" />
+                    </S.Label>
+                </S.FormGroup>
+
+                <S.LoginWrapper>
+                    <S.ForgotPassword href='#'>
+                            Esqueceu sua senha?
+                    </S.ForgotPassword>
+
+                    <S.Button href="/parent">Entrar</S.Button>
+                </S.LoginWrapper>
+            
                 
+
             </S.FormWrapper>
         </S.FormBackground>
     </S.Container>
--- src/pages/Login/sections/Form/styled.js
+++ src/pages/Login/sections/Form/styled.js
@@ -5,12 +5,22 @@ import microphone from './images/microphone.jpg'
 export const Container = styled.section`
     position: relative;
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    align-items:center;
 
     height: calc(100vh - var(--navbar-height));
     width: 100%;
 
-    background: white;
+    background: linear-gradient(to right bottom, var(--light-brown), var(--dark-brown));
+`
+
+export const Title = styled.h1`
+    margin: 0 0 0.5em 0;
+    color: white;
+    text-transform: uppercase; 
+    font-size: 4rem;
+    font-family: 'Fredoka One';
 `
 
 export const FormBackground = styled.div`
@@ -18,15 +28,90 @@ export const FormBackground = styled.div`
     width:100%;
     max-width: 30em;
 	min-height: 40em;
-	margin: auto;
 	
 	background:url(${microphone}) no-repeat center/cover;
 	box-shadow:0 12px 15px 0 rgba(0,0,0,.24),0 17px 50px 0 rgba(0,0,0,.19);
 `
+
 export const FormWrapper = styled.div`
-	position:absolute;
-    width:100%;
-	height:100%;
+	position: absolute;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+	height: 100%;
 	padding:5.5em 4em 3em 4em;
-	background:rgba(141, 104, 82,.9);
+	background:rgba(141, 104, 82,0.9);
+`
+
+export const FormGroup = styled.div`
+    margin-bottom: 1rem;
+`
+
+export const Label = styled.label`
+    font-size: 1.2rem;
+    color: white;
+`
+
+export const Field = styled.input`
+    display: block;
+    width: 100%;
+    height: 3rem;
+    padding: 0.375rem 0.75rem;
+    margin-top: 0.5rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #000;
+    background-color: rgba(255,255,255,0.4);
+    background-clip: padding-box;
+    border: none;
+    border-radius: 2rem;
+    box-shadow:0 0.5rem 1rem rgba(0, 0, 0, 0.3);
+`
+
+export const LoginWrapper = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+
+    width: 100%; 
+    margin-top: auto;
+`
+
+export const ForgotPassword = styled.a`
+    color: white;
+    font-size: 1.3em;
+    text-decoration: none;
+    margin-bottom: 2em;
+`
+
+export const Button = styled.a`
+
+	position: relative;
+	overflow: hidden;
+	align-self: center;
+    justify-self: center;
+
+    cursor: pointer;
+    background: var(--dark-brown);
+
+    padding: 0.4em;
+    border: 3px solid var(--dark-brown);
+    border-radius: 0.3em;
+
+    height: 100%;
+    width: fit-content;
+
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase; 
+    font-family: 'Bebas Neue';
+    font-size: 2.5em;
+
+    transition: background 0.5s ease-in-out, border 0.5s ease-in-out;
+
+    &:hover{
+        background: transparent;
+        border: 3px solid white;
+    }
 `

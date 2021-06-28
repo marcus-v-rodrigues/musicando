cf99c363f8a7554d104bfa626a74966ac4279e37
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2021-06-28 16:53:56 -0300

    Search product by id in ProductDetails implemented

--- src/pages/ProductDetails/index.js
+++ src/pages/ProductDetails/index.js
@@ -5,12 +5,11 @@ import * as S from './styled'
 import products from '../../data/products'
 
 const ProductDetails = ({ match }) => {
-    const product = products.find(product => product.id === match.params.id)
-    console.log(product)
+    const product = products.find(product => product.id === Number(match.params.id))
     return (
         <PageContainer>
             <S.Wrapper>
-                {product}
+                {product.name}
             </S.Wrapper>
         </PageContainer>
     )

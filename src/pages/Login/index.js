b6734d90f7078cfc3cfb1633e234cbf7ae1f9a0f
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2021-06-28 13:09:13 -0300

    Removed section in Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,39 @@
 import React from 'react'
-
-import Form from './sections/Form'
+import PageContainer from '../../components/Layout/PageContainer'
+import * as S from './styled'
 
 const Login = () => {
   return (
-    <>
-        <Form/>
-    </>
+    <PageContainer>
+        <S.Title>Login</S.Title>
+        <S.FormBackground>
+            <S.FormWrapper>
+
+                <S.FormGroup>
+                    <S.Label for="email">
+                        Email
+                    <S.Field type="email" name="email" id="email"  placeholder="email@dominio.com" />
+                    </S.Label>
+                </S.FormGroup>
+
+                <S.FormGroup>
+                    <S.Label for="password">
+                        Senha
+                    <S.Field type="password" name="password" id="password" placeholder="Sua senha" />
+                    </S.Label>
+                </S.FormGroup>
+
+                <S.LoginWrapper>
+                    <S.ForgotPassword href='#'>
+                            Esqueceu sua senha?
+                    </S.ForgotPassword>
+
+                    <S.Button href="/parent">Entrar</S.Button>
+                </S.LoginWrapper>
+
+            </S.FormWrapper>
+        </S.FormBackground>
+    </PageContainer>
   )
 }
 

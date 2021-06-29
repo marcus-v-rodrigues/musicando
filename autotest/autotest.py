ae6c03004fd13847471c9be5da185876eb99cbba
idalen <danielmartins@usp.br>
2021-06-29 15:36:56 -0300

    Works on chrome and modelated code

--- autotest/autotest.py
+++ autotest/autotest.py
@@ -1,49 +1,19 @@
+from utils import test_website
 from selenium import webdriver
-from time import sleep
 
-SCROLL_TIME = 0.01
-    
-def scroll_page_down(driver):
-    height = driver.execute_script("return document.body.scrollHeight")
-    
-    for x in range(0, height, 5):
-        driver.execute_script(f"window.scrollTo(0,{x});")
-        sleep(SCROLL_TIME)
+driver = webdriver.Chrome()
+driver.get("http://localhost:3000")
 
-def resize_window(driver):
-    window_size = driver.get_window_size()
-    height = window_size['height']
-    width =  window_size['width']
-    for n in range(1, 7):
-        driver.set_window_size(width-100*n, height-25*n)
-        sleep(3)
-    scroll_page_down(driver)
-    driver.set_window_size(width, height)
+test_website(driver)
 
+driver.close() 
 
 driver = webdriver.Firefox()
 driver.get("http://localhost:3000")
 
-element = driver.find_elements_by_class_name("sc-dIsUp.jXSUvX")
-element_link = [link.get_attribute('href') for link in element]
+test_website(driver)
 
-resize_window(driver)
-scroll_page_down(driver)
-
-for e in element_link:
-    driver.get(e)
-    sleep(1)
-
-    resize_window(driver)
-    scroll_page_down(driver)
-
-    inner_ele = driver.find_elements_by_class_name("sc-ciSkZP.dcdDki")
-    inner_links = [link.get_attribute('href') for link in inner_ele]
-    for link in inner_links:
-        driver.get(link)
-        sleep(1)
-        scroll_page_down(driver)
-        driver.back()
-    
 driver.close() 
+
+
 

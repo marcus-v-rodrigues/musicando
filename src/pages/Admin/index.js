0bdbc7102ac2318ff195a3307196a2cc9eee25bb
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2021-07-03 16:30:09 -0300

    Created Admin component

---
+++ src/pages/Admin/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageContainer from '../../components/Layout/PageContainer'
+import * as S from './styled'
+
+const Admin = () => {
+    return (
+        <PageContainer>
+            
+        </PageContainer>
+    )
+}
+
+export default Admin
